bc5b984007facac33572b700b00e4b5c018ab585
Zulfikar936077 <mdzulfikar.hasan@weavesrl.com>
2026-01-30 11:17:00 +0100

    Variables chapter

---
+++ Js/2.2variables_js.js
@@ -0,0 +1,34 @@
+/*variables = container for storing data*/
+/*1. declaration let x;
+  2. assignment x = 10;*/
+
+  let x = 10; //declaration and assignment
+  console.log(x);
+  let age = 20; //declaration and assignment
+  console.log(age);
+   let price = 10.99; //declaration and assignment
+   console.log(price);
+   let gpa = 3.8; //declaration and assignment
+ console.log(`Your gpa is ${gpa}`);
+  console.log(`Your price is ${price}`);
+  console.log(`The price is ${price} and the gpa is ${gpa}`);
+  console.log(typeof age);
+  let firstName = "John";
+  console.log(firstName);
+  console.log(typeof firstName);
+  console.log(`You name is ${firstName}`);
+  let email = "john@example.com";
+  console.log(email);
+  console.log(typeof email);
+  let online = true;
+  console.log(typeof online);
+  console.log(`John is online: ${online}`);
+  let forSale = false;
+  console.log(`This car is for sale: ${forSale}`);
+ let fullName = "John Doe";
+ let age2 = 20;
+ let student2 = true;
+
+ document.getElementById("p1").textContent = `Your name is ${fullName}`;
+ document.getElementById("p2").textContent = `Your age is ${age2}`;
+ document.getElementById("p3").textContent = `Enrolled in school: ${student2}`;
