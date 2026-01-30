f276d249e6048998b5138fa70638c3a9a22e5e8a
Zulfikar936077 <mdzulfikar.hasan@weavesrl.com>
2026-01-30 10:38:15 +0100

    Intro Js

---
+++ Js/1.2intro_js.js
@@ -0,0 +1,13 @@
+//This is a comment in JavaScript
+/*
+This is a multi-line comment in JavaScript
+*/
+
+//console.log("Hello World");
+//console.log("I like JavaScript");
+
+//window.alert("This is an alert box");
+//window.alert("This is a confirm box");
+document.getElementById("H1").textContent = "Hello H1";
+document.getElementById("p1").textContent = "Hello P1";
+
