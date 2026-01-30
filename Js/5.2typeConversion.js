a5cd13e0c5bb662cb9d0552c2a78c3d7cebf9815
Zulfikar936077 <mdzulfikar.hasan@weavesrl.com>
2026-01-30 13:57:42 +0100

    Type Conversion

---
+++ Js/5.2typeConversion.js
@@ -0,0 +1,22 @@
+/*
+type conversion = converting a value from one data type to another
+                  (e.g. string to number, number to string, etc.)
+                  Implicit = automatic conversion
+                  Explicit = manual conversion
+*/
+
+//let age = window.prompt("Enter your age:");
+//age = Number(age);
+//age += 1;
+//console.log(age, typeof age);
+
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
