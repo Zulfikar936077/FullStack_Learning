2acda486673d3bd4f215d175cd7e91669e83cb13
Zulfikar936077 <mdzulfikar.hasan@weavesrl.com>
2026-01-30 12:15:56 +0100

    arithmetic operator

---
+++ Js/3.2arithmeticOperators_js.js
@@ -0,0 +1,32 @@
+let students = 20;
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//students = students ** 2; //exponentiation
+//students %= 3; //modulus, remainder of division
+//let extraStudents = students % 3;
+//console.log(extraStudents);
+
+//augmented assignment operators
+//students += 1; //students = students + 1;
+//students -= 1; //students = students - 1;
+//students *= 2; //students = students * 2;
+//students /= 2; //students = students / 2;
+//students **= 2; //students = students ** 2;
+//students %= 3; //students = students % 3; 
+//console.log(students);
+
+//increment and decrement operators
+//students++; //students = students + 1;
+students--; //students = students - 1;
+console.log(students);
+
+/*operator precedence
+1. parentheses ()
+2. exponents **
+3. multiplication and division 
+4. addition and subtraction + -
+*/
+let result = 1 + 2 * 3 + 4 ** 2;
+console.log(result);
