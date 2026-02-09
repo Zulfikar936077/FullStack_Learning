0fadd8016f609ff1670ce3bc9d675a542c8f7615
Zulfikar936077 <mdzulfikar.hasan@weavesrl.com>
2026-02-09 15:34:31 +0100

    Ts updated

--- Ts/Learn TS in 50 mins/dist/main.js
+++ Ts/Learn TS in 50 mins/dist/main.js
@@ -1,3 +1,13 @@
+/*
+The topics covered here are:
+1. Env setup
+2. Variable Declarations
+3. Variable types
+4. Functions
+5. Interface
+6. Classes
+7. Access modifiers
+*/
 let message = "Welcome to TypeScript";
 console.log(message);
 let x = 10;
@@ -26,4 +36,63 @@ var color;
 ;
 let c = color.Green;
 console.log(c);
+let randomValue = 10;
+randomValue = true;
+randomValue = "Optimistics";
+console.log(randomValue);
+/*let myVariable: any = 10;
+myVariable();
+myVariable.toUpperCase();
+
+/*let myvariable: unknown =10;
+ function hasName(obj: any): obj is {name:string} {
+    return !!obj && typeof obj === "object" && "name" in obj;
+ }
+
+ //if(hasName(myvariable)){
+   // console.log(myvariable.name);
+//}
+ 
+    //console.log((myvariable as {name: string}).name.toUpperCase());
+ */
+let multiType;
+multiType = 10;
+multiType = true;
+console.log(multiType);
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(1, 2);
+console.log(add(1, 2));
+function fullName(person) {
+    console.log(`${person.firstname} ${person.lastName}`);
+}
+let p = {
+    firstname: "John",
+    lastName: "Doe",
+};
+fullName(p);
+class Employee {
+    constructor(name) {
+        this.employeeName = name;
+    }
+    greet() {
+        console.log(`Hello ${this.employeeName}`);
+    }
+}
+let emp1 = new Employee("John");
+console.log(emp1.employeeName);
+emp1.greet();
+class Manager extends Employee {
+    constructor(managerName) {
+        super(managerName);
+    }
+    delegateWork() {
+        console.log(`Manager delegating tasks`);
+    }
+}
+let m = new Manager("Jane");
+m.greet();
+m.delegateWork();
+console.log(m.employeeName);
 export {};
--- Ts/Learn TS in 50 mins/main.ts
+++ Ts/Learn TS in 50 mins/main.ts
@@ -1,3 +1,15 @@
+/*
+The topics covered here are:
+1. Env setup
+2. Variable Declarations
+3. Variable types
+4. Functions
+5. Interface
+6. Classes
+7. Access modifiers
+*/
+
+
 export {}
 let message = "Welcome to TypeScript";
 console.log(message);
@@ -28,3 +40,84 @@ let list3: [number, string] = [1, "cookie"];
 enum color {Red =5, Green, Blue};
 let c: color = color.Green;
 console.log(c);
+
+let randomValue: any =10;
+randomValue = true;
+randomValue = "Optimistics";
+console.log(randomValue);
+
+/*let myVariable: any = 10;
+myVariable();
+myVariable.toUpperCase();
+
+/*let myvariable: unknown =10;
+ function hasName(obj: any): obj is {name:string} {
+    return !!obj && typeof obj === "object" && "name" in obj;
+ }
+
+ //if(hasName(myvariable)){
+   // console.log(myvariable.name);
+//}
+ 
+    //console.log((myvariable as {name: string}).name.toUpperCase());
+ */
+
+    let multiType: number | boolean;
+    multiType = 10;
+    multiType = true;
+    console.log(multiType);
+
+
+    function add(num1: number, num2: number): number{
+        return num1 + num2;
+    }
+    add(1,2);
+    console.log(add(1,2));
+
+
+
+
+interface Person {
+    firstname: string;
+    lastName: string;
+}
+  function fullName(person: Person){
+    console.log(`${person.firstname} ${person.lastName}`);
+  }
+  let p ={
+    firstname: "John",
+    lastName: "Doe",
+  }
+fullName(p);
+
+
+class Employee {
+    public employeeName: string;
+
+    constructor(name: string){
+        this.employeeName = name;
+    }
+    greet(){
+        console.log(`Hello ${this.employeeName}`);
+    }
+}
+
+let emp1 = new Employee("John");
+console.log(emp1.employeeName);
+emp1.greet();
+
+
+class Manager extends Employee {
+    constructor(managerName: string){
+        super(managerName);
+    }
+    delegateWork(){
+        console.log(`Manager delegating tasks`);
+    }
+}
+
+let m = new Manager("Jane");
+m.greet();
+m.delegateWork();
+console.log(m.employeeName);
+
