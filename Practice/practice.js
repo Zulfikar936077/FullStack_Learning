418feea9235e40036dd5f78e568ec19466a78835
Zulfikar936077 <mdzulfikar.hasan@weavesrl.com>
2026-02-19 12:36:52 +0100

    DOM Challenge

--- Practice/practice.js
+++ Practice/practice.js
@@ -55,7 +55,7 @@ function whosPaying(names) {
   var selectedPerson = names[randomIndex];
   return selectedPerson + " is going to buy lunch today.";
 }
-=======
+
 var output = [];
 var count = 1;
 
