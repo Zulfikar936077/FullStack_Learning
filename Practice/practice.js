b10287779619c293c16d1e24dfde3c207f734024
Zulfikar936077 <mdzulfikar.hasan@weavesrl.com>
2026-02-19 10:37:11 +0100

    again

--- Practice/practice.js
+++ Practice/practice.js
@@ -25,10 +25,6 @@ if(input % 3 === 0 && input % 5 === 0){
 */
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 14cdc3de37f269e906c06d2ecd5d3a7ea6020ac4
 /*
 var output = [];
 var count = 1;
@@ -53,7 +49,6 @@ for (var i = 0; i < 100; i++) {
 }
 */
 
-<<<<<<< HEAD
 var names = ["Angela", "Ben", "Jenny", "Michael", "Chole"];
 function whosPaying(names) {
   var randomIndex = Math.floor(Math.random() * names.length);
@@ -64,27 +59,22 @@ function whosPaying(names) {
 var output = [];
 var count = 1;
 
-function fizzBuzz(){
-  while(count<=100){
-    if(count % 3 === 0 && count % 5 === 0){
-      output.push("FizzBuzz");
-    }else if(count % 5 === 0){
-      output.push("Buzz");
-    }else if(count % 3 === 0){
-      output.push("Fizz");
-    }else {
-      output.push(count);
-    }
-    count++;
+function fizzBuzz() {
+  if (count % 3 === 0 && count % 5 === 0) {
+    output.push("FizzBuzz");
+  } else if (count % 5 === 0) {
+    output.push("Buzz");
+  } else if (count % 3 === 0) {
+    output.push("Fizz");
+  } else {
+    output.push(count);
   }
+  count++;
   console.log(output);
 }
 
-
->>>>>>> 14cdc3de37f269e906c06d2ecd5d3a7ea6020ac4
-
-
-
-
-
+// Call it 100 times in one run
+for (var i = 0; i < 100; i++) {
+  fizzBuzz();
+}
 
