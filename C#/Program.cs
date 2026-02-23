9bc1bc2ce286f1c82eb5d9b68e6c92db787418fc
Zulfikar936077 <mdzulfikar.hasan@weavesrl.com>
2026-02-23 09:33:38 +0100

    C#

---
+++ C#/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+using System;
+
+namespace MyFirstProgram
+    {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I like pizza!");
+            Console.WriteLine("It's really good!");
+        }
+    }
+}
