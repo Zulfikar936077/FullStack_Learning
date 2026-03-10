ce5263d1ef396e1417aaf6c276683bbb2d449ad4
Zulfikar936077 <mdzulfikar.hasan@weavesrl.com>
2026-03-10 10:53:30 +0100

    Adding button in Embedded Analytics

--- Tableau/Embedded Analytics by Andre/app.js
+++ Tableau/Embedded Analytics by Andre/app.js
@@ -1,67 +1,49 @@
-console.log("I am here!");
+console.log("I am a script");
 let viz;
 const containerDiv = document.getElementById("vizContainer");
 const btn = document.getElementById("btn");
 const showBtn = document.getElementById("showBtn");
 const exportPDF = document.getElementById("exportPDF");
 const exportImage = document.getElementById("exportImage");
-const url =
-  "https://public.tableau.com/views/LearnEmbeddedAnalytics/SalesOverviewDashboard";
+const url = 'https://public.tableau.com/views/LearnEmbeddedAnalytics/SalesOverviewDashboard?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link';
 const options = {
   hideTabs: true,
   height: 800,
-  width: 1000,
+  width: 1900,
+  device: 'desktop',
   onFirstInteractive: function() {
-    console.log("Hey, my dashboard is interactive!");
+    console.log('Hey, my dashboard is ready!');
   },
   onFirstVizSizeKnown: function() {
-    console.log("Hey, my dashboard has a size!");
+    console.log('The viz size is known!');
   }
-};
-
+  }
 function initViz() {
-  viz = new tableau.Viz(containerDiv, url, options);
+  viz = new tableau.Viz(containerDiv, url, options); //V should be in capital letter in Viz because it is a class
 }
 
-document.addEventListener("DOMContentLoaded", initViz);
-// listen for clicks to hide the viz
-btn.addEventListener("click", function() {
-  console.log("Hello from my button!");
+document.addEventListener('DOMContentLoaded', initViz);
+btn.addEventListener('click' , function() {
+  console.log('Button clicked!');
   viz.hide();
-});
-// listen for clicks to show the viz
-showBtn.addEventListener("click", function() {
+})
+//listen for clicks to hide the viz
+btn.addEventListener('click' , function() {
+  console.log('Button clicked!');
+  viz.hide();
+})
+//listen for clicks to show the viz
+showBtn.addEventListener('click', function(){
+  console.log('Show button clicked!');
   viz.show();
-});
-// listen for clicks to export to PDF
-exportPDF.addEventListener("click", function() {
+})
+//listen for clicks to export the viz as a PDF
+exportPDF.addEventListener('click', function(){
+  console.log('Export PDF button clicked!');
   viz.showExportPDFDialog();
-});
-
-// listen for click to export an Image
-exportImage.addEventListener("click", function() {
+})
+//listen for clicks to export the viz as an image
+exportImage.addEventListener('click', function(){
+  console.log('Export Image button clicked!');
   viz.showExportImageDialog();
-});
-
-function getRangeValues() {
-  // get the values from the input
-  const minValue = document.getElementById("minValue").value;
-  const maxValue = document.getElementById("maxValue").value;
-  // get the workbook object
-  const workbook = viz.getWorkbook();
-  // get the active sheet in the window - this is the dashboard
-  const activeSheet = workbook.getActiveSheet();
-  // get all the sheets in the dashboard
-  const sheets = activeSheet.getWorksheets();
-  const sheetToFilter = sheets[1];
-  sheetToFilter
-    .applyRangeFilterAsync("Sales", {
-      min: minValue,
-      max: maxValue
-    })
-    .then(console.log("Filter applied!"));
-}
-
-document.getElementById("applyFilter").addEventListener("click", function() {
-  getRangeValues();
-});
+})
