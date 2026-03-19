f74acab0c68f77d285fd70232171df21e7e1d447
Zulfikar936077 <mdzulfikar.hasan@weavesrl.com>
2026-03-19 11:06:56 +0100

    Defining input variable in Angular

--- Angular/01-starting-project/src/app/user/user.component.ts
+++ Angular/01-starting-project/src/app/user/user.component.ts
@@ -1,6 +1,6 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
+import { Component, Input, input} from '@angular/core';
+// import { DUMMY_USERS } from '../dummy-users';
+// const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,8 +11,8 @@ const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
 })
 export class UserComponent {
   //selectedUser = DUMMY_USERS[randomIndex];  //for zone.js
-  selectedUser = signal(DUMMY_USERS[randomIndex]); //1. initializing signal
-  imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);  //using computed function as it is going to change with the selectedUser signal and to avoid getter and setter.
+  // selectedUser = signal(DUMMY_USERS[randomIndex]); //1. initializing signal
+  // imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);  //using computed function as it is going to change with the selectedUser signal and to avoid getter and setter.
   //getter and setter are used for zone.js
   /*
   get imagePath() {   //getter is used to get the selected user's avatar path
@@ -20,9 +20,19 @@ export class UserComponent {
   } //this is used as it is using the selectedUser property which is within the same class
    */
 
+  // This code defines an input property called 'avatar' for the UserComponent.
+  // The '@Input()' decorator tells Angular that this property can receive its value from a parent component.
+  // The 'avatar!: string;' part declares that 'avatar' is a required string property.
+  @Input() avatar!: string;
+  @Input() name!: string;
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
+    //const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
     //this.selectedUser = DUMMY_USERS[randomIndex];
-    this.selectedUser.set(DUMMY_USERS[randomIndex]); //2. updating signal
+    //this.selectedUser.set(DUMMY_USERS[randomIndex]); //2. updating signal
+  
   }
 }
