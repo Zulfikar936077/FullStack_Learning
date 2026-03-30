2b9cbdb8cc33dae2d0355279d72c6078edc1bb2c
Zulfikar936077 <mdzulfikar.hasan@weavesrl.com>
2026-03-30 13:25:01 +0200

    Using Signal Inputs_Angular_Completed

--- C#/HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ C#/HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3dcec93ad4424fbaa9d1dabce17d61b14da0ce2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b381669362ca7c279986db921f6a877b400521f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
